b9609b9f6b39bee1cd153801245b1e6d4fc37c2a
lrn <liurennan1996@163.com>
2017-10-30 13:11:23 +0800

    项目文件

---
+++ src/main/resources/logistics.sql
@@ -0,0 +1,140 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.11 : Database - logistics
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`logistics` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `logistics`;
+
+/*Table structure for table `driver` */
+
+DROP TABLE IF EXISTS `driver`;
+
+CREATE TABLE `driver` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `gender` varchar(10) CHARACTER SET latin1 DEFAULT NULL,
+  `photo` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `licenseNumber` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `state` varchar(11) CHARACTER SET latin1 DEFAULT NULL,
+  `plateNumber` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+/*Data for the table `driver` */
+
+insert  into `driver`(`id`,`name`,`gender`,`photo`,`licenseNumber`,`state`,`plateNumber`) values (2,'2','0','2','22','0','2'),(3,'3','1','3','3','1','3'),(4,'4','1','4','4','1','4'),(5,'5','0','5','5','1','5'),(6,'6','1','6','6','0','6'),(7,'7','0','7','7','0','7'),(8,'8','1','8','8','0','8'),(9,'9','1','9','9','0','9'),(10,'10','0','10','10','0','10'),(11,'11','0','11','11','0','11'),(15,'a','0','a','b','1','b'),(16,'b','0','b','b','1','b'),(26,'c','0','b','f','1','c');
+
+/*Table structure for table `goods` */
+
+DROP TABLE IF EXISTS `goods`;
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `consignor` varchar(50) DEFAULT NULL,
+  `consignee` varchar(50) DEFAULT NULL,
+  `consignor_phone` varchar(100) DEFAULT NULL,
+  `consignee_phone` varchar(100) DEFAULT NULL,
+  `consignee_adress` varchar(200) DEFAULT NULL,
+  `consignor_adress` varchar(200) DEFAULT NULL,
+  `current_address` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `goods` */
+
+/*Table structure for table `permission` */
+
+DROP TABLE IF EXISTS `permission`;
+
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(200) DEFAULT NULL,
+  `permission` varchar(200) DEFAULT NULL,
+  `permission_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `permission` */
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `role` */
+
+/*Table structure for table `truck` */
+
+DROP TABLE IF EXISTS `truck`;
+
+CREATE TABLE `truck` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate_number` varchar(50) DEFAULT NULL,
+  `deadweight` varchar(50) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `truck` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `latestDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+/*Table structure for table `user_permission` */
+
+DROP TABLE IF EXISTS `user_permission`;
+
+CREATE TABLE `user_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `permission_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_permission` */
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_role` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
